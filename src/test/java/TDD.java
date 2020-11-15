5813c98dd79e85f34903739c1a5988e444527be5
半透明的墙 <kscbxxLiuXP@126.com>
2020-11-15 20:47:19 +0800

    init

---
+++ src/test/java/TDD.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertThat;
+
+public class TDD {
+
+    @Test
+    public void testGreetNormalSingleStringName() {
+
+        String name = "bob";
+        TDD tdd = new Tdd();
+        String returnValue  = tdd.greet(name);
+
+        assert 
+    }
+}
